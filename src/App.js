1a02676f158c8d45571fed14e632296f2ccc909c
AndreLimaS <a.s.l0@hotmail.com>
2020-10-30 17:36:22 -0300

    useContext Login

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
-import './App.css'
+import "./App.css";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header/>
-        <Routes> 
-           <Route path="/" element={<Home/>}/>
-           <Route path="/login/*" element={<Login/>}/>
-
-        </Routes>
-        <Footer/>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
