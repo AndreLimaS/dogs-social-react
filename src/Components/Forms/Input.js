826105785edafa0bdd48acb5cffd664044c02839
AndreLimaS <a.s.l0@hotmail.com>
2020-10-30 12:35:24 -0300

    validate login

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import styles from './Input.module.css';
+import React from "react";
+import styles from "./Input.module.css";
 
-function Input({label, type, name}) {
+function Input({ label, type, name, value, onChange, error, onBlur }) {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name}>{label}</label>
-      <input id={name} name={name} className={styles.input} type={type}></input>
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      ></input>
+      {error && <p className={styles.error}>{error}</p>}
     </div>
-      
-  )
+  );
 }
 
-export default Input
+export default Input;
