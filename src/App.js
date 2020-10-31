6c12554f932005806da7e8ce6b1f4bb1567e3fb9
AndreLimaS <a.s.l0@hotmail.com>
2020-10-31 01:38:07 -0300

    validate token

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
