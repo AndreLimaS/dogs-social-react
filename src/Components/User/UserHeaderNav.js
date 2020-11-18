99417a1789e1c69768e2e22435d014048e212437
AndreLimaS <a.s.l0@hotmail.com>
2020-11-18 16:21:05 -0300

    feat: create components conta and lougout

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
 const UserHeaderNav = () => {
-  return <nav>Navegacao</nav>;
+  const { userLogout } = React.useContext(UserContext);
+
+  return (
+    <nav>
+      <NavLink to="/conta">Minhas Fotos</NavLink>
+      <NavLink to="/conta/estatisticas">Estatisticas</NavLink>
+      <NavLink to="/conta">Adicionar Foto</NavLink>
+      <button onClick={userLogout}>Sair</button>
+    </nav>
+  );
 };
 
 export default UserHeaderNav;
