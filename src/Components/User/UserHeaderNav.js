a6ead14ef2f433b5887976512dae6ff574147e17
AndreLimaS <a.s.l0@hotmail.com>
2020-11-24 09:53:02 -0300

    feat: style menuMobile

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 import useMedia from "../../Hooks/useMedia";
 
@@ -14,6 +14,11 @@ const UserHeaderNav = () => {
   const mobile = useMedia("(max-width: 40rem)");
   const [mobileMenu, setMobileMenu] = React.useState(false);
 
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
     <>
       {mobile && (
@@ -25,7 +30,11 @@ const UserHeaderNav = () => {
           onClick={() => setMobileMenu(!mobileMenu)}
         ></button>
       )}
-      <nav className={styles.nav}>
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
         <NavLink to="/conta" end activeClassName={styles.active}>
           <MinhasFotos />
           {mobile && "Minhas Fotos"}
